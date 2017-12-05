5ce1993f71de2f44faaf7d88be4c8f9fed4f64ab
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2017-12-05 19:19:46 +0000

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
-(module info (lib "infotab.ss" "setup")
+#lang info
   (define name "mzlite")
   (define blurb
     (list "A DrScheme language level for a minimal mzscheme."))
   (define doc.txt "doc.txt")
   (define categories '(misc metaprogramming))
   (define version "1")
-  (define primary-file "mzlite.ss"))
+  (define primary-file "mzlite.ss")
